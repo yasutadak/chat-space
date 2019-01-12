f23097addffe2f4345f57e433e38d5048c4f44d7
yasutadak <yasutada629@gmail.com>
2019-01-12 14:50:28 +0900

    グループのユーザー名の表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+
   def index
   end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @groups = Group.all
   end
 
   def create
