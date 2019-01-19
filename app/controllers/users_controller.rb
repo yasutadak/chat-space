b593f9a4acd890e8f368e42d940bfd86ba8e295d
yasutadak <yasutada629@gmail.com>
2019-01-19 17:36:05 +0900

    users_controller.rbの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
   def search
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
-      format.html
       format.json
     end
   end
